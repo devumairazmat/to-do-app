7c4299cfc5cb5655193069be400ed1f80b88e007
devumairazmat <umairazmat8260@gmail.com>
2022-08-04 07:09:53 +0500

    Local Storage added

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import NavScrollExample from "./components/navabr/Navbar";
 import Footer from "./components/footer/Footer";
 import Todos from "./components/Todos";
-import React, { useState } from "react";
+import AddToDo from "./components/AddToDo";
 
 function App() {
+  
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  // Add todo
+  const addTodo = (title, description) => {
+    console.log("More To Do's Added", title, description);
+    let id;
+    if (todos.length == 0) {
+      id = 1;
+    } else {
+      id = todos[todos.length - 1].id + 1;
+    }
+
+    const myTodo = {
+      id: id,
+      title: title,
+      description: description,
+    };
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  };
+  // Delete todo
   const onDelete = (todo) => {
     console.log("Delete this To do Task", todo);
     setTodos(
       todos.filter((e) => {
-        return e!== todo;
+        return e !== todo;
       })
     );
+    localStorage.getItem("todos");
   };
+  
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Web Development",
-      description:
-        "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services",
-    },
-    {
-      id: 2,
-      title: "Mobile App Development",
-      description:
-        "Mobile app development is the act or process by which a mobile app is developed for mobile devices, such as personal digital assistants, enterprise digital assistants or mobile phones. These software applications are designed to run on mobile devices, such as a smartphone or tablet computer",
-    },
-    {
-      id: 3,
-      title: "Desktop Applictaion Development",
-      description:
-        "Desktop application development is a type of software programming that creates computer programs to be run on the desktop. The most common use case for these applications is business-oriented and provides certain functionality such as word processing, spreadsheets, or project management tools",
-    },
-  ]);
-
+ 
   return (
     <>
       <NavScrollExample title="My Todos App" searchBar="false" />
+      <AddToDo addTodo={addTodo} />
       <Todos todos={todos} onDelete={onDelete} />
       <Footer />
     </>
---
+++ src/components/AddToDo.js
@@ -0,0 +1,42 @@
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import React, { useState } from "react";
+
+function AddToDo(props) {
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+
+    const submit = (e) =>{
+        e.preventDefault();
+        if(!title || !description ){
+            alert("Titel and Description should not be blank!");
+        }
+        else{
+            props.addTodo(title, description);
+            setTitle("");
+            setDescription("");
+        }
+        
+    }
+  return (
+   <Container>
+     <Form onSubmit={submit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <h3 className="my-4 text-center">Add To Do's</h3>
+        <Form.Label>Title</Form.Label>
+        <Form.Control value={title} onChange={(e)=> setTitle(e.target.value)} type="text" placeholder="Enter Title" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Description</Form.Label>
+        <Form.Control value={description} onChange={(e)=> setDescription(e.target.value)} type="text" placeholder="Description" />
+      </Form.Group>
+      <Button variant="success" size="sm" type="submit">
+        Add To Do
+      </Button>
+    </Form>
+   </Container>
+  );
+}
+
+export default AddToDo;
